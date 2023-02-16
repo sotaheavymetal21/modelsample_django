45aff55e31ec854530069de780b3c9ed5751b4c5
sotaheavymetal21 <euro246seeker@gmail.com>
2023-02-16 22:09:57 +0900

    migrationの練習

--- ModelApp/models.py
+++ ModelApp/models.py
@@ -6,3 +6,9 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+
+
+class Sales(models.Model):
+    fee = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
